cdeb2e273a45b5b0bf08955c9539b2ba8bb465dc
Sreehari <sreehari1672000@gmail.com>
2023-09-28 10:51:58 +0530

    Initial Commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <div className='App-header'></div>
+      <div className='App-body'></div>
+      <div className='App-input'></div>
+    </div>
+  );
+}
+
+export default App;
